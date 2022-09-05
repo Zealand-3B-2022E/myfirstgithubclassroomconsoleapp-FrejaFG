6ec58003a509476a52511fd10b8ecd3a49390a06
Freja Gudmandsen <ferling.gud@gmail.com>
2022-09-05 12:51:55 +0200

    Democlass+prop+instance

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class Demo
+    {
+        private int _myNumber;
+        private int _myHeight;
+        private string? _myPlants;
+
+        public int MyNumber { get =>  _myNumber ; set => _myNumber=value; }
+
+        public int MyHeight { get => _myHeight ; set => _myHeight=value; }
+
+        public string MyPlants { get; set; } = "Not enough";
+
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+
+
+
+
+    }
+}
